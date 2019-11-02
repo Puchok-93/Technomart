6950e3cd71a4bf3fe067608930cbe446244e9b74
Puchok-93 <paul.puchkov@yandex.ru>
2019-11-03 00:10:24 +0300

    Работа над базовыми и дополнительными критериями

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /*Товар добавлен в корзину"*/
 var cartLink = document.querySelector(".buy"); 
 var cartPopup= document.querySelector(".cart-popup");
@@ -12,20 +11,6 @@ var cartClose = cartPopup.querySelector(".modal-close");
     cartClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     cartPopup.classList.remove("cart-popup-show");
-});
-
-/*Вызов кнопки "Написать нам"*/
-var link = document.querySelector(".write-us-button"); 
-var writeUsForm = document.querySelector(".write-us-form");
-var close = writeUsForm.querySelector(".modal-close");
-    link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    writeUsForm.classList.add("write-us-form-show");
-});
-
-    close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    writeUsForm.classList.remove("write-us-form-show");
 });
 
 /*Вызов карты*/
@@ -42,3 +27,32 @@ var mapClose = mapPopup.querySelector(".modal-close");
     evt.preventDefault();
     mapPopup.classList.remove("modal-map-show");
   });
+/*Вызов кнопки "Написать нам"*/
+var link = document.querySelector(".write-us-button"); 
+var writeUsForm = document.querySelector(".write-us-form");
+var close = writeUsForm.querySelector(".modal-close");
+
+var form = writeUsForm.querySelector("form");
+var name = writeUsForm.querySelector("[name=user-name]");
+var email = writeUsForm.querySelector("[name=email]");
+
+    link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    writeUsForm.classList.add("write-us-form-show");
+});
+
+    close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    writeUsForm.classList.remove("write-us-form-show");
+    writeUsForm.classList.remove("modal-error");
+});
+
+  form.addEventListener("submit", function (evt) {
+    form.classList.remove("modal-error")
+    if (!username.value || !email.value) {
+    evt.preventDefault();
+    form.classList.remove("modal-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("modal-error");
+  }
+});
